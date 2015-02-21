c84a7a3caf4c79b08cd2c4315db5c324853736b9
Jérôme WIRSZTEL <jerome.wirsztel@epitech.eu>
2015-02-20 18:45:19 -0800

    Checked upload toolow

--- app.js
+++ app.js
@@ -84,7 +84,11 @@ app.get('/', function(req, res) {
 });
 
 app.get('/upload/:name', function(req, res) {
-  upload(dirPath + '/' + req.params.name, function() {
+  if (isTooLow(req.query.currentPath)) {
+    res.status(403);
+    res.end();
+  }
+  upload(dirPath + '/' + purgePath(req.query.currentPath) + '/' + req.params.name, function() {
     res.status(200);
     res.end();
   });
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,23 @@ $('.uploadBtn').click(function() {
     return ;
   }
   btn.html('<span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>');
-  $.get('/upload/' + $(this).attr('id'), function(data) {
+  $.get('/upload/' + $(this).attr('id') + '?currentPath=' + getUrlParameter('path'), function(data) {
     btn.html('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>');
   }).fail(function(data) {
     btn.html('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
   });
 });
+
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}
